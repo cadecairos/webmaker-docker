88fc893a21980dc1fa20a5fc6f6820d6a65d0665
Christopher De Cairos <cade@mozillafoundation.org>
2015-11-20 17:00:39 -0500

    typo

--- data-services/run-data-services.sh
+++ data-services/run-data-services.sh
@@ -7,7 +7,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     export PG_VOLUME_PATH=./pg_data
     export MARIADB_VOLUME_PATH=./mariadb_data
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    export "WINDOWS DETECTED"
+    echo "WINDOWS DETECTED"
     export PG_VOLUME_PATH=/c/Users/$USER/.config/webmaker/pg_data
     export MARIADB_VOLUME_PATH=/c/USERS/$USER/.config/webmaker/mariadb_data
 fi
