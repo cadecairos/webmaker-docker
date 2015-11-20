6d5fbe435aa4c86c3082dc5de7fa43cb296f2d12
Christopher De Cairos <cade@mozillafoundation.org>
2015-11-20 17:35:04 -0500

    WINDOWS

--- data-services/run-data-services.sh
+++ data-services/run-data-services.sh
@@ -8,8 +8,8 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     export MARIADB_VOLUME_PATH=./mariadb_data
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
     echo "WINDOWS DETECTED"
-    export PG_VOLUME_PATH=/c/Users/$USER/.config/webmaker/pg_data
-    export MARIADB_VOLUME_PATH=/c/Users/$USER/.config/webmaker/mariadb_data
+    export PG_VOLUME_PATH=$HOMEPATH/.config/webmaker/pg_data
+    export MARIADB_VOLUME_PATH=$HOMEPATH/.config/webmaker/mariadb_data
 fi
 
 docker-compose up $1
